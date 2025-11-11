97d9a1288e1ef129ea3191db7290405e5b08435f
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2025-11-11 15:47:51 +0100

    chore: Un-deprecate `k_EAccountTypeConsoleUser` for now, too many things use it

--- src/public/steam/steamclientpublic.h
+++ src/public/steam/steamclientpublic.h
@@ -203,7 +203,7 @@ enum EAccountType {
 	k_EAccountTypeContentServer = 6, // content server
 	k_EAccountTypeClan = 7,
 	k_EAccountTypeChat = 8,
-	k_EAccountTypeConsoleUser [[deprecated]] = 9,// Fake SteamID for local PSN account on PS3 or Live account on 360, etc.
+	k_EAccountTypeConsoleUser/* [[deprecated]]*/ = 9,// Fake SteamID for local PSN account on PS3 or Live account on 360, etc.
 	k_EAccountTypeAnonUser = 10,
 
 	// Max of 16 items in this field
