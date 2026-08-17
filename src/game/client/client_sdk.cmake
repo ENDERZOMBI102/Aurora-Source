ec8e1035105d92367b8bad58c885857e67bd78d9
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2026-08-17 21:47:50 +0200

    feat: Client game dll cmake files!

--- src/game/client/client_sdk.cmake
+++ src/game/client/client_sdk.cmake
@@ -1,103 +1,97 @@
 # client_sdk.cmake
-include( "${CMAKE_CURRENT_LIST_DIR}/client_base.cmake" )
+set( PROJECT "client_sdk" )
 
-set( CLIENT_SDK_DIR ${CMAKE_CURRENT_LIST_DIR} )
-set( CLIENT_SDK_SOURCE_FILES
+list( APPEND SOURCE_FILES
 	# Source Files
-	"${CLIENT_SDK_DIR}/c_team_objectiveresource.cpp"
-	"${CLIENT_SDK_DIR}/c_team_objectiveresource.h"
-	"${CLIENT_SDK_DIR}/c_team_train_watcher.cpp"
-	"${CLIENT_SDK_DIR}/c_team_train_watcher.h"
-	"${CLIENT_SDK_DIR}/hud_voicestatus.cpp"
-	"${SRCDIR}/game/shared/predicted_viewmodel.cpp"
-	"${SRCDIR}/game/shared/predicted_viewmodel.h"
-	"${SRCDIR}/game/shared/teamplay_round_timer.cpp"
-	"${SRCDIR}/game/shared/teamplay_round_timer.h"
+	"${CLIENT_DIR}/c_team_objectiveresource.cpp"
+	"${CLIENT_DIR}/c_team_objectiveresource.h"
+	"${CLIENT_DIR}/c_team_train_watcher.cpp"
+	"${CLIENT_DIR}/c_team_train_watcher.h"
+	"${CLIENT_DIR}/hud_voicestatus.cpp"
+	"${SRC_DIR}/game/shared/predicted_viewmodel.cpp"
+	"${SRC_DIR}/game/shared/predicted_viewmodel.h"
+	"${SRC_DIR}/game/shared/teamplay_round_timer.cpp"
+	"${SRC_DIR}/game/shared/teamplay_round_timer.h"
 
 	# HL2 DLL
-	"${SRCDIR}/game/shared/script_intro_shared.cpp"
+	"${SRC_DIR}/game/shared/script_intro_shared.cpp"
 
 	# SDK
-	"${CLIENT_SDK_DIR}/sdk/c_sdk_env_sparkler.cpp"
-	"${CLIENT_SDK_DIR}/sdk/c_sdk_player.cpp"
-	"${CLIENT_SDK_DIR}/sdk/c_sdk_player.h"
-	"${CLIENT_SDK_DIR}/sdk/c_sdk_team.cpp"
-	"${CLIENT_SDK_DIR}/sdk/c_sdk_team.h"
-	"${CLIENT_SDK_DIR}/sdk/c_te_firebullets.cpp"
-	"${CLIENT_SDK_DIR}/sdk/c_weapon_stubs_midn.cpp"
-	"${CLIENT_SDK_DIR}/sdk/clientmode_sdk.cpp"
-	"${CLIENT_SDK_DIR}/sdk/clientmode_sdk.h"
-	"${CLIENT_SDK_DIR}/sdk/sdk_fx_impacts.cpp"
-	"${CLIENT_SDK_DIR}/sdk/sdk_fx_weaponfx.cpp"
-	"${CLIENT_SDK_DIR}/sdk/sdk_hud_ammo.cpp"
-	"${CLIENT_SDK_DIR}/sdk/sdk_hud_health.cpp"
-	"${CLIENT_SDK_DIR}/sdk/sdk_hud_message.cpp"
-	"${CLIENT_SDK_DIR}/sdk/sdk_hud_weaponselection.cpp"
-	"${CLIENT_SDK_DIR}/sdk/sdk_in_main.cpp"
-	"${CLIENT_SDK_DIR}/sdk/sdk_prediction.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_gamerules.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_gamerules.h"
-	"${SRCDIR}/game/shared/sdk/sdk_player_shared.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_player_shared.h"
-	"${SRCDIR}/game/shared/sdk/sdk_weapon_parse.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_weapon_parse.h"
-	"${SRCDIR}/game/shared/sdk/sdk_weapon_melee.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_weapon_melee.h"
-	"${SRCDIR}/game/shared/sdk/sdk_usermessages.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_playeranimstate.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_playeranimstate.h"
-	"${SRCDIR}/game/shared/sdk/sdk_fx_shared.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_fx_shared.h"
-	"${SRCDIR}/game/shared/sdk/sdk_basegrenade_projectile.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_basegrenade_projectile.h"
-	"${SRCDIR}/game/shared/sdk/sdk_gamemovement.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_playerclass_info_parse.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_playerclass_info_parse.h"
-	"${SRCDIR}/game/shared/sdk/sdk_shareddefs.cpp"
-	"${SRCDIR}/game/shared/sdk/sdk_shareddefs.h"
+	"${CLIENT_DIR}/sdk/c_sdk_env_sparkler.cpp"
+	"${CLIENT_DIR}/sdk/c_sdk_player.cpp"
+	"${CLIENT_DIR}/sdk/c_sdk_player.h"
+	"${CLIENT_DIR}/sdk/c_sdk_team.cpp"
+	"${CLIENT_DIR}/sdk/c_sdk_team.h"
+	"${CLIENT_DIR}/sdk/c_te_firebullets.cpp"
+	"${CLIENT_DIR}/sdk/c_weapon_stubs_midn.cpp"
+	"${CLIENT_DIR}/sdk/clientmode_sdk.cpp"
+	"${CLIENT_DIR}/sdk/clientmode_sdk.h"
+	"${CLIENT_DIR}/sdk/sdk_fx_impacts.cpp"
+	"${CLIENT_DIR}/sdk/sdk_fx_weaponfx.cpp"
+	"${CLIENT_DIR}/sdk/sdk_hud_ammo.cpp"
+	"${CLIENT_DIR}/sdk/sdk_hud_health.cpp"
+	"${CLIENT_DIR}/sdk/sdk_hud_message.cpp"
+	"${CLIENT_DIR}/sdk/sdk_hud_weaponselection.cpp"
+	"${CLIENT_DIR}/sdk/sdk_in_main.cpp"
+	"${CLIENT_DIR}/sdk/sdk_prediction.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_gamerules.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_gamerules.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_player_shared.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_player_shared.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_weapon_parse.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_weapon_parse.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_weapon_melee.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_weapon_melee.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_usermessages.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_playeranimstate.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_playeranimstate.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_fx_shared.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_fx_shared.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_basegrenade_projectile.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_basegrenade_projectile.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_gamemovement.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_playerclass_info_parse.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_playerclass_info_parse.h"
+	"${SRC_DIR}/game/shared/sdk/sdk_shareddefs.cpp"
+	"${SRC_DIR}/game/shared/sdk/sdk_shareddefs.h"
 
 	# SDK->vgui
-	"${CLIENT_SDK_DIR}/sdk/vgui/sdkviewport.cpp"
-	"${CLIENT_SDK_DIR}/sdk/vgui/sdkviewport.h"
-	"${CLIENT_SDK_DIR}/sdk/vgui/vgui_rootpanel_sdk.cpp"
-	"${CLIENT_SDK_DIR}/sdk/vgui/vgui_rootpanel_sdk.h"
+	"${CLIENT_DIR}/sdk/vgui/sdkviewport.cpp"
+	"${CLIENT_DIR}/sdk/vgui/sdkviewport.h"
+	"${CLIENT_DIR}/sdk/vgui/vgui_rootpanel_sdk.cpp"
+	"${CLIENT_DIR}/sdk/vgui/vgui_rootpanel_sdk.h"
 
 	# SDK->Weapons
-	"${SRCDIR}/game/shared/sdk/weapon_sdkbase.cpp"
-	"${SRCDIR}/game/shared/sdk/weapon_sdkbase.h"
-	"${SRCDIR}/game/shared/sdk/weapon_basesdkgrenade.cpp"
-	"${SRCDIR}/game/shared/sdk/weapon_basesdkgrenade.h"
-	"${SRCDIR}/game/shared/sdk/weapon_crowbar.cpp"
-	"${SRCDIR}/game/shared/sdk/weapon_grenade.cpp"
-	"${SRCDIR}/game/shared/sdk/weapon_grenade.h"
-	"${SRCDIR}/game/shared/sdk/weapon_mp5.cpp"
-	"${SRCDIR}/game/shared/sdk/weapon_pistol.cpp"
-	"${SRCDIR}/game/shared/sdk/weapon_shotgun.cpp"
+	"${SRC_DIR}/game/shared/sdk/weapon_sdkbase.cpp"
+	"${SRC_DIR}/game/shared/sdk/weapon_sdkbase.h"
+	"${SRC_DIR}/game/shared/sdk/weapon_basesdkgrenade.cpp"
+	"${SRC_DIR}/game/shared/sdk/weapon_basesdkgrenade.h"
+	"${SRC_DIR}/game/shared/sdk/weapon_crowbar.cpp"
+	"${SRC_DIR}/game/shared/sdk/weapon_grenade.cpp"
+	"${SRC_DIR}/game/shared/sdk/weapon_grenade.h"
+	"${SRC_DIR}/game/shared/sdk/weapon_mp5.cpp"
+	"${SRC_DIR}/game/shared/sdk/weapon_pistol.cpp"
+	"${SRC_DIR}/game/shared/sdk/weapon_shotgun.cpp"
 )
 
-set( CLIENT_SDK_EXCLUDE_SOURCES
-	"${SRCDIR}/game/shared/weapon_parse_default.cpp"
-)
+# Set the output file name
+set( OUTPUT_FILE_NAME "client" )
+# Set the output file location
+set( OUTPUT_FILE_DIR "${GAME_DIR}/mod_sdk/bin" )
 
-add_library( client_sdk MODULE ${CLIENT_SDK_SOURCE_FILES} )
+# Target type
+set( KIND "MODULE" )
 
-set_target_properties( client_sdk
-	PROPERTIES
-		OUTPUT_NAME "client"
-		PREFIX ""
-		LIBRARY_OUTPUT_DIRECTORY "${GAMEDIR}/mod_sdk/bin"
-)
+# Include this to handle all the defines and actually create the target
+include( "${CMAKESCRIPTS_DIR}/project-base.cmake" )
 
-target_use_client_base( client_sdk CLIENT_SDK_EXCLUDE_SOURCES )
-
-target_include_directories( client_sdk
+asource_include_directories(
 	PRIVATE
-		"${CLIENT_SDK_DIR}/sdk/vgui"
-		"${CLIENT_SDK_DIR}/sdk"
-		"${SRCDIR}/game/shared/sdk"
+		"${CLIENT_DIR}/sdk/vgui"
+		"${CLIENT_DIR}/sdk"
+		"${SRC_DIR}/game/shared/sdk"
 )
-
-target_compile_definitions( client_sdk
+asource_compile_definitions(
 	PRIVATE
 		SDKDLL
 )
