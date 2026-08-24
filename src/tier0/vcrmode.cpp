ee3066dbf38d53b377a900d2e50e3274295477e3
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2026-08-24 22:21:44 +0200

    feat: Add required vprof_telemetry and vcrmode globals

---
+++ src/tier0/vcrmode.cpp
@@ -0,0 +1,8 @@
+//
+// Created by ENDERZOMBI102 on 24/08/2026.
+//
+#include "tier0/vcrmode.h"
+
+
+// TODO: All decls in this file should depend on `NO_VCR`
+VCR_t* g_pVCR{};
---
+++ src/tier0/vprof_telemetry.cpp
@@ -0,0 +1,14 @@
+//
+// Created by ENDERZOMBI102 on 24/08/2026.
+//
+#include "tier0/vprof_telemetry.h"
+
+
+#if !defined( RAD_TELEMETRY_ENABLED )
+	int g_Telemetry[70] { };
+#else
+
+#endif
+
+
+
